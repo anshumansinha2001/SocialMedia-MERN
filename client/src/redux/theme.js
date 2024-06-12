e37d1f15bf939c76c0b569e39306430cafb3ac93
Anshuman Sinha <anshumansinha2001@gmail.com>
2024-06-12 08:03:40 +0530

    Initial commit

---
+++ client/src/redux/theme.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  theme: (() => {
+    try {
+      return JSON.parse(window?.localStorage.getItem("theme")) || "dark";
+    } catch (e) {
+      return "dark";
+    }
+  })(),
+};
+
+// const initialState = {
+//   theme: JSON.parse(window?.localStorage.getItem("theme")) ?? "dark",
+// };
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    setTheme(state, action) {
+      state.theme = action.payload;
+      localStorage.setItem("theme", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export default themeSlice.reducer;
+
+export function SetTheme(value) {
+  return (dispatch) => {
+    dispatch(themeSlice.actions.setTheme(value));
+  };
+}
